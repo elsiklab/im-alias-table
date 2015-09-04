d2a28ef88040516fd4377ec0ef939a7cbd0a41b2
Colin <colin.diesh@gmail.com>
2015-09-04 13:24:59 -0500

    Add bulk links

--- index.js
+++ index.js
@@ -11,10 +11,69 @@ if(param) {
 
 
 
+function runQuery(query) {
 
-function runQuery(gene_id) {
     var element = $('#my-id');
     var service = {root: 'http://www.bovinegenome.org/bovinemine'};
+    
+    // Configure options here, using nested notation
+    imtables.configure({TableCell: {PreviewTrigger: 'click'}});
+    // Or using path names:
+    imtables.configure('TableResults.CacheFactor', 20);
+
+    // Then load the table (or indeed vice-versa, the table
+    // will respond to changes in the options)
+    imtables.loadTable(
+        element, // Could also be a string or a jquery object
+        {start: 0, size: 25}, // Can be null - all properties are optional.
+        {service: service, query: query} // Can also be an imjs.Query object
+    ).then(
+        function handleTable (table) {
+            $("#message").html("Powered by <a href='http://bovinegenome.org/bovinemine/'>BovineMine</a>");
+            var links=document.querySelectorAll("a");
+            for(var i=0;i<links.length;i++)
+            {
+               links[i].setAttribute("target","_blank");
+            }
+        },
+        function reportError (error) { console.error('Could not load table', error); }
+    );
+}
+
+
+function runQuery(query) {
+    var element = $('#my-id');
+    var service = {root: 'http://www.bovinegenome.org/bovinemine'};
+    
+    // Configure options here, using nested notation
+    imtables.configure({TableCell: {PreviewTrigger: 'click'}});
+    // Or using path names:
+    imtables.configure('TableResults.CacheFactor', 20);
+
+    // Then load the table (or indeed vice-versa, the table
+    // will respond to changes in the options)
+    imtables.loadTable(
+        element, // Could also be a string or a jquery object
+        {start: 0, size: 25}, // Can be null - all properties are optional.
+        {service: service, query: query} // Can also be an imjs.Query object
+    ).then(
+        function handleTable (table) {
+            $("#message").html("Powered by <a href='http://bovinegenome.org/bovinemine/'>BovineMine</a>");
+            var links=document.querySelectorAll("a");
+            for(var i=0;i<links.length;i++)
+            {
+               links[i].setAttribute("target","_blank");
+            }
+        },
+        function reportError (error) { console.error('Could not load table', error); }
+    );
+}
+
+$("form").submit(function() {
+    var gene_id = $('#gene_id').val();
+    if(!gene_id) {
+        gene_id=$('#ens_gene_id').val();
+    }
     var query={
         "model":{"name":"genomic"},
         "select":[
@@ -32,29 +91,49 @@ function runQuery(gene_id) {
             {"path":"Gene.primaryIdentifier","op":"=","code":"A","value":gene_id}
         ]
     };
-    // Configure options here, using nested notation
-    imtables.configure({TableCell: {PreviewTrigger: 'click'}});
-    // Or using path names:
-    imtables.configure('TableResults.CacheFactor', 20);
-
-    // Then load the table (or indeed vice-versa, the table
-    // will respond to changes in the options)
-    imtables.loadTable(
-        element, // Could also be a string or a jquery object
-        {start: 0, size: 25}, // Can be null - all properties are optional.
-        {service: service, query: query} // Can also be an imjs.Query object
-    ).then(
-        function handleTable (table) { $("#message").html("Powered by <a href='http://bovinegenome.org/bovinemine/'>BovineMine</a>"); },
-        function reportError (error) { console.error('Could not load table', error); }
-    );
-}
-
-$("form").submit(function() {
-    var gene_id = $('#gene_id').val();
-    if(!gene_id) {
-        gene_id=$('#ens_gene_id').val();
-    }
-    runQuery(gene_id);
+    runQuery(query);
     return false;
 });
 
+
+$("#ens_all").on('click',function() {
+    var query={
+        "model":{"name":"genomic"},
+        "select":[
+            "Gene.primaryIdentifier",
+            "Gene.secondaryIdentifier",
+            "Gene.symbol",
+            "Gene.name",
+            "Gene.source",
+            "Gene.alias.primaryIdentifier",
+            "Gene.alias.source"
+        ],
+        "constraintLogic":"A and B",
+        "orderBy":[{"Gene.primaryIdentifier":"ASC"}],
+        "where":[{"path":"Gene.source","op":"=","code":"A","value":"Ensembl Data Set"}]
+    };
+    runQuery(query);
+    return false;
+});
+
+
+
+$("#ncbi_all").on('click',function() {
+    var query={
+        "model":{"name":"genomic"},
+        "select":[
+            "Gene.primaryIdentifier",
+            "Gene.secondaryIdentifier",
+            "Gene.symbol",
+            "Gene.name",
+            "Gene.source",
+            "Gene.alias.primaryIdentifier",
+            "Gene.alias.source"
+        ],
+        "constraintLogic":"A and B",
+        "orderBy":[{"Gene.primaryIdentifier":"ASC"}],
+        "where":[{"path":"Gene.source","op":"IS NULL","code":"A","value":"IS NULL"}]
+    };
+    runQuery(query);
+    return false;
+});

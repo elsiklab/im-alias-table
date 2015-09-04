b6fffa4605ee8e57336e65682f68abe5c1d1bbf9
Colin <colin.diesh@gmail.com>
2015-09-04 12:44:58 -0500

    Replace intermine.css with im-tables main.css

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ function runQuery(gene_id) {
         "constraintLogic":"A and B",
         "orderBy":[{"Gene.primaryIdentifier":"ASC"}],
         "where":[
-            {"path":"Gene.primaryIdentifier","op":"=","code":"A","value":gene_id},
-            {"path":"Gene.alias.source","op":"=","code":"B","value":"Ensembl-Gene-Set"}
+            {"path":"Gene.primaryIdentifier","op":"=","code":"A","value":gene_id}
         ]
     };
     // Configure options here, using nested notation
@@ -49,50 +48,13 @@ function runQuery(gene_id) {
         function reportError (error) { console.error('Could not load table', error); }
     );
 }
-function runQueryEnsembl(gene_id) {
-    var element = $('#my-id');
-    var service = {root: 'http://www.bovinegenome.org/bovinemine'};
-    var query={
-        "model":{"name":"genomic"},
-        "select":[
-            "Gene.primaryIdentifier",
-            "Gene.secondaryIdentifier",
-            "Gene.symbol",
-            "Gene.name",
-            "Gene.source",
-            "Gene.alias.primaryIdentifier",
-            "Gene.alias.source"
-        ],
-        "orderBy":[{"Gene.primaryIdentifier":"ASC"}],
-        "where":[{"path":"Gene.primaryIdentifier","op":"=","code":"A","value":gene_id}]
-    }
-    // Configure options here, using nested notation
-    imtables.configure({TableCell: {PreviewTrigger: 'click'}});
-    // Or using path names:
-    imtables.configure('TableResults.CacheFactor', 20);
-
-    // Then load the table (or indeed vice-versa, the table
-    // will respond to changes in the options)
-    imtables.loadTable(
-        element, // Could also be a string or a jquery object
-        {start: 0, size: 25}, // Can be null - all properties are optional.
-        {service: service, query: query} // Can also be an imjs.Query object
-    ).then(
-        function handleTable (table) { $("#message").html("Powered by <a href='http://bovinegenome.org/bovinemine/'>BovineMine</a>"); },
-        function reportError (error) { console.error('Could not load table', error); }
-    );
-}
-
 
 $("form").submit(function() {
     var gene_id = $('#gene_id').val();
     if(!gene_id) {
         gene_id=$('#ens_gene_id').val();
-        runQueryEnsembl(gene_id);
     }
-    else {
-        runQuery(gene_id);
-    }
+    runQuery(gene_id);
     return false;
 });
 
